5d7d04d327ce99580d5b2d7dda317d835e54fb42
李帅 <1735612628@qq.com>
2022-09-16 21:09:37 +0800

    feat: 认证完成之后才会加载页面

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,30 +1,36 @@
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import '../styles/app.scss'
 import AxiosInstance from '../utils/aixos/axios'
 export default function MyApp({ Component, pageProps }: AppProps) {
   const router = useRouter()
+  const [isPending, setIsPending] = useState(true);
   useEffect(() => {
     AxiosInstance.get('/auth/isLogin')
-    .then((res) => {
-      if (res.data.msg === 'false') {
-        router.push('/auth/login')
-      } else if (res.data.msg === 'true') {
-        if (router.pathname === '/auth/login') {
-          router.push('/')
+      .then(async (res) => {
+        if (res.data.msg === 'false') {
+          await router.push('/auth/login')
+          setIsPending(false)
+        } else if (res.data.msg === 'true') {
+          if (router.pathname === '/auth/login') {
+            router.push('/')
+          }
+          setIsPending(false)
         }
-      }
-    })
-    .catch((err) => {
-      router.push('/auth/login')
-    })
+      })
+      .catch(async (err) => {
+        await router.push('/auth/login')
+        setIsPending(false)
+      })
   }, [])
   return (<div>
     <Head>
       <title>聊天室</title>
     </Head>
-    <Component {...pageProps} />
+    {
+      !isPending && <Component {...pageProps} />
+    }
   </div>)
 }
