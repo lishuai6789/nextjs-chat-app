c3f9d3c29ca9ba1e56741b5e4f874d4533a57d2c
李帅 <1735612628@qq.com>
2022-09-18 11:54:04 +0800

    update

--- components/index/FomDialog.tsx
+++ components/index/FomDialog.tsx
@@ -1,21 +1,26 @@
-import { Dialog, DialogTitle, DialogContent, TextField, DialogActions, Button, Input } from "@mui/material"
-import { ChangeEvent, memo, useState, useMemo, useRef, FormEvent } from "react"
+import { Dialog, DialogTitle, DialogContent, TextField, DialogActions, Button, } from "@mui/material"
+import LoadingButton from '@mui/lab/LoadingButton';
+import { ChangeEvent, memo, useState, useMemo, useRef, FormEvent, ReactElement, MouseEvent } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { RootState } from "./store"
 import { closeProfile } from "./uiSlice"
 import { client } from "../../utils/alioss/alioss"
-const FormDialog = memo(function FormDialog() {
-  const toggle: boolean = useSelector((state: RootState) => state.ui.toggleProfile)
-  const username: string = useSelector((state: RootState) => state.user.username)
+import SendIcon from '@mui/icons-material/Send';
+import styles from '../../styles/FormDialog.module.scss'
+import AxiosInstance from "../../utils/aixos/axios";
+import { AxiosError, AxiosResponse } from "axios";
+
+interface PropsInterface {
+  username: string;
+};
+
+const ModifyNickname = memo(function ModifyNickname(props: PropsInterface): ReactElement {
   const nickname: string = useSelector((state: RootState) => state.user.nickname)
-  const signature: string = useSelector((state: RootState) => state.user.signature)
-  const dispatch = useDispatch()
-  const handleCloseProfile = () => {
-    dispatch(closeProfile())
-  }
-  const [formNickname, setFormNickname] = useState({ val: nickname, isError: false, helperText: '' });
-  const [formSignature, setFormSignature] = useState({ val: signature, isError: false, helperText: '' });
-  const [formAvatarHelpertText, setFormAvatarHelperText] = useState({ helperText: '', isError: false });
+  const [formNickname, setFormNickname] = useState({
+    val: nickname,
+    isError: nickname.length <= 0 || nickname.length > 20,
+    helperText: ''
+  });
   const handleNickname = (event: ChangeEvent<HTMLInputElement>) => {
     let newV = event.target.value.trim();
     if (newV.length === 0) {
@@ -26,6 +31,63 @@ const FormDialog = memo(function FormDialog() {
       setFormNickname({ val: newV, helperText: '', isError: false })
     }
   }
+  const [status, setStatus] = useState(0) // 0 初始状态；1 加载中；2 成功；3 错误
+  const color = useMemo((): "primary" | "success" | "error" | "inherit" | "secondary" | "info" | "warning" => {
+    if (status === 0 || status === 1) {
+      return "primary"
+    } else if (status === 2) {
+      return "success"
+    } else {
+      return "error"
+    }
+  }, [status])
+  const handleSubmit = (): void => {
+    if (formNickname.isError) {
+      return;
+    }
+    setStatus(1)
+    AxiosInstance.post('/profile/updateNickname', {
+      username: props.username,
+      nickname: formNickname.val
+    })
+      .then((res: AxiosResponse) => {
+        setStatus(2)
+      })
+      .catch((err: any) => {
+        setStatus(3)
+      })
+  }
+  return (
+    <div className={styles.container}>
+      <TextField
+        fullWidth
+        autoComplete="off"
+        label="昵称"
+        type="text"
+        value={formNickname.val}
+        variant="standard"
+        onChange={handleNickname}
+        error={formNickname.isError}
+        helperText={formNickname.helperText}></TextField>
+      <LoadingButton
+        variant="contained"
+        color={color}
+        onClick={handleSubmit}
+        loadingPosition="end"
+        size="small"
+        loading={status === 1}
+        endIcon={<SendIcon />}>提交</LoadingButton>
+    </div>
+  )
+})
+
+const ModifySignature = memo(function ModifySignature(props: PropsInterface): ReactElement {
+  const signature: string = useSelector((state: RootState) => state.user.signature)
+  const [formSignature, setFormSignature] = useState({
+    val: signature,
+    isError: signature.length <= 0 || signature.length > 30,
+    helperText: ''
+  });
   const handleSignature = (event: ChangeEvent<HTMLInputElement>) => {
     let newV = event.target.value.trim();
     if (newV.length <= 0 || newV.length >= 31) {
@@ -42,7 +104,61 @@ const FormDialog = memo(function FormDialog() {
       })
     }
   }
+  const [status, setStatus] = useState(0) // 0 初始状态；1 加载中；2 成功；3 错误
+  const color = useMemo((): "primary" | "success" | "error" | "inherit" | "secondary" | "info" | "warning" => {
+    if (status === 0 || status === 1) {
+      return "primary"
+    } else if (status === 2) {
+      return "success"
+    } else {
+      return "error"
+    }
+  }, [status])
+  const handleSubmit = (): void => {
+    if (formSignature.isError) {
+      return;
+    }
+    setStatus(1)
+    AxiosInstance.post('/profile/updateSignature', {
+      username: props.username,
+      nickname: formSignature.val
+    })
+      .then((res: AxiosResponse) => {
+        if (res.data.code !== 100) {
+          console.log("success")
+          setStatus(2)
+        }
+      })
+      .catch((err: any) => {
+        setStatus(3)
+      })
+  }
+  return (
+    <div className={styles.container}>
+      <TextField autoComplete="off"
+        variant="standard"
+        fullWidth
+        type="text"
+        label="个性签名"
+        value={formSignature.val}
+        onChange={handleSignature}
+        helperText={formSignature.helperText}
+        error={formSignature.isError}></TextField>
+      <LoadingButton
+        variant="contained"
+        color={color}
+        onClick={handleSubmit}
+        loadingPosition="end"
+        size="small"
+        loading={status === 1}
+        endIcon={<SendIcon />}> 提交</LoadingButton>
+    </div >
+  )
+})
+
+const MofiyAvatar = memo(function MofiyAvatar(props: PropsInterface): ReactElement {
   const avatarRef = useRef(null)
+  const [formAvatarHelpertText, setFormAvatarHelperText] = useState({ helperText: '', isError: false });
   const handleAvatar = (event: ChangeEvent<HTMLInputElement>) => {
     let files = event.target.files;
     if (!files[0].type.includes("image")) {
@@ -57,35 +173,40 @@ const FormDialog = memo(function FormDialog() {
       })
     }
   }
-  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    if (formNickname.isError || formSignature.isError || formAvatarHelpertText.isError) {
-      return;
-    }
+  return (
+    <div>
+      <div className="mb-3">
+        <label htmlFor="formFileLg" className="form-label">新头像</label>
+        <input ref={avatarRef} required className="form-control form-control-sm" id="formFileLg" type="file" onChange={handleAvatar} accept="image/*" />
+        {
+          formAvatarHelpertText.isError && <div style={{ padding: '5px' }} className="alert alert-danger" role="alert">
+            {formAvatarHelpertText.helperText}
+          </div>
+        }
+      </div>
+    </div>
+  )
+})
+
+const FormDialog = memo(function FormDialog(): ReactElement {
+  const toggle: boolean = useSelector((state: RootState) => state.ui.toggleProfile)
+  const username: string = useSelector((state: RootState) => state.user.username)
+  const dispatch = useDispatch();
+  const handleCloseProfile = (): void => {
+    dispatch(closeProfile())
   }
   return (
     <Dialog open={toggle} onClose={handleCloseProfile}>
-      <form encType="multipart/form-data" onSubmit={handleSubmit}>
-        <DialogTitle>修改用户信息</DialogTitle>
-        <DialogContent>
-          <TextField disabled required value={username} variant="standard" fullWidth label="用户名"></TextField>
-          <TextField fullWidth required autoComplete="off" label="昵称" type="text" value={formNickname.val} variant="standard" onChange={handleNickname} error={formNickname.isError} helperText={formNickname.helperText}></TextField>
-          <TextField required autoComplete="off" variant="standard" fullWidth type="text" label="个性签名" value={formSignature.val} onChange={handleSignature} helperText={formSignature.helperText} error={formSignature.isError}></TextField>
-          <div className="mb-3">
-            <label htmlFor="formFileLg" className="form-label">新头像</label>
-            <input ref={avatarRef} required className="form-control form-control-sm" id="formFileLg" type="file" onChange={handleAvatar} accept="image/*" />
-            {
-              formAvatarHelpertText.isError && <div style={{ padding: '5px' }} className="alert alert-danger" role="alert">
-                {formAvatarHelpertText.helperText}
-              </div>
-            }
-          </div>
-        </DialogContent>
-        <DialogActions>
-          <Button variant="contained" type="button" onClick={handleCloseProfile}>取消</Button>
-          <Button variant="contained" type="submit">提交</Button>
-        </DialogActions>
-      </form>
+      <DialogTitle>修改用户信息</DialogTitle>
+      <DialogContent>
+        <TextField disabled value={username} variant="standard" fullWidth label="用户名(不可以修改！！)"></TextField>
+        <ModifyNickname username={username} />
+        <ModifySignature username={username} />
+        <MofiyAvatar username={username} />
+      </DialogContent>
+      <DialogActions>
+        <Button variant="contained" type="button" onClick={handleCloseProfile}>取消</Button>
+      </DialogActions>
     </Dialog>
   )
 })
